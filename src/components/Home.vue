d2af28646aaa174b5affdea722c83c8ec3fabbcb
Timothy.Smith <timothy.smith@motius.local>
2019-08-11 21:46:05 +0200

    btr content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,64 @@
 <template>
     <div>
-        <h1>Hello World</h1>
-        <p>Photo 1</p>
+      <!-- Navbar -->
+      <div>
+        <b-navbar type="dark" variant="dark">
+          <b-navbar-nav>
+            <b-nav-item href="#">HOME</b-nav-item>
+            <b-nav-item href="#">BAND</b-nav-item>
 
-        <div>
-            <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-                <p>For more information visit website</p>
-                <b-button variant="primary" href="#">More Info</b-button>
-            </b-jumbotron>
-        </div>
-    </div>
+            <!-- Navbar dropdowns -->
+            <b-nav-item-dropdown text="PROGRAMM" right>
+              <b-dropdown-item href="#">SEKTEMPFANG</b-dropdown-item>
+              <b-dropdown-item href="#">DINNERMUSIK</b-dropdown-item>
+              <b-dropdown-item href="#">TANZ & PARTY</b-dropdown-item>
+              <b-dropdown-item href="#">AFTER PARTY DJ</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item href="#">GALLERIE</b-nav-item>
+            <b-nav-item href="#">KONTAKT</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar>
+      </div><!-- Navbar -->
+
+      <!-- jumbotron -->
+      <div>
+        <b-jumbotron header="BRASS TACKS" lead="LIVE BAND">
+            <p>IHRE HOCHZEITSBAND</p>
+        </b-jumbotron>
+      </div><!-- jumbotron -->
+
+    <b-container>
+      <!-- Row of columns -->
+      <b-row>
+        <!-- Cont1 -->
+        <b-col>
+          <h2>SEKTEMPFANG</h2>
+          <p>Arsch. </p>
+          <b-btn>View Details &raquo;</b-btn>
+        </b-col>
+        <!-- Cont2 -->
+        <b-col>
+          <h2>DINNERMUSIK</h2>
+          <p>Arsch. </p>
+          <b-btn>View Details &raquo;</b-btn>
+        </b-col>
+        <!-- Cont3 -->
+        <b-col>
+          <h2>TANZ & PARTY</h2>
+            <p>Arsch. </p>
+            <b-btn>View Details &raquo;</b-btn>
+        </b-col>
+        <!-- Cont4 -->
+        <b-col>
+          <h2>AFTER PARTY DJ</h2>
+          <p>Arsch. </p>
+          <b-btn>View Details &raquo;</b-btn>
+        </b-col>
+      </b-row>
+    </b-container>
+    
+  </div>
 </template>
 
 <script>
